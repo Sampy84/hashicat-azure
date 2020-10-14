40915360483c858234b99fd1fe3bd0a7a98ed3f7
Polo Buzzi <paolo.buzzi1984@gmail.com>
2020-10-14 14:34:34 +0000

    Added the first tag

--- main.tf
+++ main.tf
@@ -8,7 +8,6 @@ resource "azurerm_resource_group" "myresourcegroup" {
   
   tags = {
     environment = "Production"
-    department = "devops"
   }
 }
 
@@ -98,6 +97,10 @@ resource "azurerm_virtual_machine" "catapp" {
 
   network_interface_ids         = [azurerm_network_interface.catapp-nic.id]
   delete_os_disk_on_termination = "true"
+
+  tags = {
+    department = "devops"
+  }
 
   storage_image_reference {
     publisher = var.image_publisher
